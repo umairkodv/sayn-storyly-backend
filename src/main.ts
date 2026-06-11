f5e980057bfd609faec0951eb6b496697b5e8956
umairkodv <umairzkodv@gmail.com>
2026-06-11 11:39:43 -0700

    reverting tehe changes

--- src/main.ts
+++ src/main.ts
@@ -41,10 +41,8 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
     credentials: true,
   });
-  
- // FIXED: Explicitly specify '0.0.0.0' as the host parameter
-  const port = process.env.PORT || 10000;
-  await app.listen(port, '0.0.0.0');
+
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
